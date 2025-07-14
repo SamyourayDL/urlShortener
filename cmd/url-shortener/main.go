38d851f5717e0476eba83fbe8ffa102d17437803
samyouray <samyouraydl@gmail.com>
2025-07-14 09:22:13 +0400

    added logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"os"
 	"url-shortener/internals/config"
 )
 
-const ()
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	cfg := config.MustLoad()
 	fmt.Println(cfg)
 
-	//init logger
-
+	log := SetupLogger(cfg.Env)
+	log.Info("Logger succesfully setuped", "Env", cfg.Env)
+	log.Debug("Debug messages are enabled")
 	//init storage
 
 	//init router
@@ -24,8 +30,16 @@ func main() {
 func SetupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
-	case "local":
-
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New( //LevelInfo, don't want to see debug messages on server
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
 
 	return log
